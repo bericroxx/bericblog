ef97a19af67605973722028f24898d0cbebfada6
Bericpirate <bericspam@gmail.com>
2013-01-11 15:09:15 +0100

    change

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define( "DB_DSN", "something");
-define( "DB_USERNAME", "yourusername" );
+define( "DB_USERNAME", "hfghfg" );
 define( "DB_PASSWORD", "yourpassword" );
 define( "PAGE_SIZE", 5 );
 define( "TBL_", "something" );
